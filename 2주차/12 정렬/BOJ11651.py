59f6fcd6b3c1eb1eacb22020a9a212ddfba9f1b5
지예 데스크탑 <hyo37009@gmail.com>
2022-01-14 14:47:48 +0900

    단순선택정렬 시간초과

--- 2주차/12 정렬/BOJ11651.py
+++ 2주차/12 정렬/BOJ11651.py
@@ -1,12 +1,12 @@
 ## 좌표 정렬하기 2 ##
 '''
 난이도 : 중하
-사용 : 버블정렬
-이게..되네?
-list가 중첩됐을 때의 index가 헷갈려서 처음에 작동이 안 됐다.
-마지막 출력문을 작성할 때 알아채서 바로 바꿨다.
-근데 시간초과 ㅎㅎ
-for문을 이렇게 잔뜩 넣었으니 예상은 했다만 다시해야지
+사용 : 단순선택정렬
+버블이 아니라 선택정렬로 하니 훨씬 효율적인 코드가 나왔다.
+근데 또 시간초과다
+...
+질문란을 보니 다른 알고리즘을 쓰라는 것 같다
+그럼 오늘의 풀이는 여기까지...
 '''
 
 
@@ -16,29 +16,15 @@ x = [None] * n
 for i in range(n):
     x[i] = list(map(int, input().split(' ')))
 
-#y좌표 먼저 정렬합니다.
-for i in range(n):
-    last = i
-    for j in range(n-1, i, -1):
-        if x[j][1] < x[j-1][1]:
-            x[j], x[j-1] = x[j-1], x[j]
-            j = last
-    i = last
-
-#x좌표를 정렬합니다.
-ran = [None, None]
-for i in range(n):
-    for j in range(n-1): #정렬할 범위를 추려냅니다.
-        if x[j][1] == x[j+1][1]:
-            if ran[0] == None:
-                ran[0] = i
-            ran[1] = j + 1
-
-    for k in range(ran[1] - 1, ran[0], -1):
-        if x[k][0] < x[k-1][0]:
-            x[k], x[k-1] = x[k-1], x[k]
-
-    ran = [None, None]
+for i in range(n-1):
+    min = i
+    for j in range(i+1, n):
+        if x[j][1] < x[min][1]:
+            min = j
+        elif x[j][1] == x[min][1]:
+            if x[j][0] < x[min][0]:
+                min = j
+    x[i], x[min] = x[min], x[i]
 
 for i in range(n):
     print(f'{x[i][0]} {x[i][1]}')
