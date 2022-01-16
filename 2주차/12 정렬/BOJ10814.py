3b57d8005a56dd3e9821c62b45ed69578b9db2bf
지예 데스크탑 <hyo37009@gmail.com>
2022-01-16 18:36:00 +0900

    sort() 맞음

--- 2주차/12 정렬/BOJ10814.py
+++ 2주차/12 정렬/BOJ10814.py
@@ -1,8 +1,10 @@
 ## 나이순 정렬 ##
 '''
 난이도 : 하
-사용 : 버블정렬
-버블 개선해보았다 ㅜㅜ 근데 시간초과 뜨지 않을까 .. 아~~
+사용 : 내부함수
+몰라서 내부함수 썼다. 근데 시간초과?뜨겠지?
+와!!!!!!!!!!!!!
+하다가 톡방에 다 못 할 것 같다고 올리고 산책하고 왔는데 채점창에 맞았습니다 떠있었다 ㅜㅜ
 '''
 n = int(input())
 member = []
@@ -10,19 +12,8 @@ member = []
 for i in range(n):
     member.append(list(input().split(' ')))
     member[i][0] = int(member[i][0])
-last, count = 0, 0
 
-for i in range(n):
-    last = i
-    for j in range(n-1, i, -1):
-        if member[j][0] < member[j-1][0]:
-            member[j], member[j-1] = member[j-1], member[j]
-            last = j
-            count += 1
-
-    i = last
-    if count == 0:
-        break
+member.sort(key= lambda x:x[0])
 
 for i in range(n):
     print(f'{member[i][0]} {member[i][1]}')
