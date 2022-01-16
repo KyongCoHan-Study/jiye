8e087917599f38250a9d58e4b4de2fe475fe71a0
지예 데스크탑 <hyo37009@gmail.com>
2022-01-16 17:12:56 +0900

    시간초과

--- 2주차/12 정렬/BOJ11651.py
+++ 2주차/12 정렬/BOJ11651.py
@@ -1,30 +1,62 @@
 ## 좌표 정렬하기 2 ##
 '''
-난이도 : 중하
-사용 : 단순선택정렬
-버블이 아니라 선택정렬로 하니 훨씬 효율적인 코드가 나왔다.
-근데 또 시간초과다
-...
-질문란을 보니 다른 알고리즘을 쓰라는 것 같다
-그럼 오늘의 풀이는 여기까지...
+퀵소트로... 시간초과 뜰 것 같던데 시간초과떴다..ㅎㅎ
+모르겟다
 '''
 
 
+def quickSort(a: list, n: int):
+    Range = []
+    Range2 = []
+    pl = 0
+    pr = n - 1
+    Range.append((pl, pr))
+    x = a[n // 2][1]
+
+    while Range:
+        tem = Range.pop()
+        pl, pr = left, right = tem[0], tem[1]
+        x = a[(left + right) // 2][1]
+
+        while pl <= pr:
+            while a[pl][1] < x: pl += 1
+            while a[pr][1] > x: pr -= 1
+            if pl < pr:
+                a[pl], a[pr] = a[pr], a[pl]
+                pl += 1
+                pr -= 1
+        if left < pr: Range.append((left, pr))
+        if pl < right: Range.append((pl, right))
+
+        if a[pl][1] == a[pr][1]:  # 가운데 그룹이 존재하는 경우
+            pl2 = pr
+            pr2 = pl
+            Range2.append((pl2, pr2))
+            while Range2:
+                tem2 = Range2.pop()
+                pl2, pr2 = left2, right2 = tem2[0], tem2[1]
+                x2 = a[(left2 + right2) // 2][0]
+
+                while pl2 <= pr2:
+                    while a[pl2][0] < x: pl2 += 1
+                    while a[pr2][0] > x: pr2 -= 1
+                    if pl2 < pr2:
+                        a[pl2], a[pr2] = a[pr2], a[pl2]
+                        pl2 += 1
+                        pr2 -= 1
+                if left2 < pr2: Range.append((left2, pr2))
+                if pl2 < right2: Range.append((pl2, right2))
+
+
 n = int(input())
-x = [None] * n
+a = [None] * n
+Range = []
+RangeX = []
 
 for i in range(n):
-    x[i] = list(map(int, input().split(' ')))
+    a[i] = list(map(int, input().split(' ')))
 
-for i in range(n-1):
-    min = i
-    for j in range(i+1, n):
-        if x[j][1] < x[min][1]:
-            min = j
-        elif x[j][1] == x[min][1]:
-            if x[j][0] < x[min][0]:
-                min = j
-    x[i], x[min] = x[min], x[i]
+quickSort(a, n)
 
 for i in range(n):
-    print(f'{x[i][0]} {x[i][1]}')
+    print(f'{a[i][0]} {a[i][1]}')
