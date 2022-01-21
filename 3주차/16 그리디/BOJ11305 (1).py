e677a54e3d246ac0e6855e1be5d2dbfb6eae09d7
지예 데스크탑 <hyo37009@gmail.com>
2022-01-21 19:01:37 +0900

    그리디

---
+++ 3주차/16 그리디/BOJ11305 (1).py
@@ -0,0 +1,24 @@
+## 주유소 ##
+'''
+난이도 :
+사용 : 그리디
+'''
+import in11305
+import sys
+in11305.ini()
+
+sys.stdin = open("in11305.txt", "rt")
+
+n = int(input())
+length = [int(i) for i in input().split()]
+cost = [int(i) for i in input().split()]
+totalCost = 0
+last = 0
+i = 0
+k = 0
+
+tem = {}
+for i in range(n):
+    tem[n]
+
+print(tem)
