fcecd56c496f2a874ff19e500bc849e6548c0241
지예 데스크탑 <hyo37009@gmail.com>
2022-01-22 19:04:16 +0900

    해결

---
+++ 3주차/14 DFS(백트래킹)/BOJ15651.py
@@ -0,0 +1,23 @@
+## N과 M (3) ##
+
+
+def dfs(L):
+
+    if L == m:
+        for j in range(m):
+            print(res[j], end=' ')
+        print()
+        return
+
+
+    else:
+        for i in range(1, n+1):
+            res[L] = i
+            dfs(L + 1)
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    res = [0] * m
+    cnt = 0
+    dfs(0)
