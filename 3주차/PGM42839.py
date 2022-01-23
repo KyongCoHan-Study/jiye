69a14870d39231898caa16abf07579befb85ae99
지예 데스크탑 <hyo37009@gmail.com>
2022-01-23 20:00:16 +0900

    통과!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 내힘으로통과!!!!!!!!!!!!!!!!

--- 3주차/PGM42839.py
+++ 3주차/PGM42839.py
@@ -1,7 +1,7 @@
 import math
 
 
-def dfs(n, d, nd):
+def dfs(d, nd):
     '''
     n : 현재 숫자
     d : depth (수열의 길이)
@@ -9,25 +9,25 @@ def dfs(n, d, nd):
     '''
     global numlist
     if nd == d:
-        numlist.append('')
-        for i in range(len(ch)):
-            if ch[i] == 1:
-                numlist[-1] += innum[i]
-        return
-    if sum(ch[:n]) + len(ch[n:]) < d:
+        numlist.append(0)
+        for i in range(len(innum)):
+            if ch[i] is not None:
+                numlist[-1] += int(innum[i]) * (10 ** ch[i])
         return
 
-    else:  # 상태트리
-        # 사용하는 경우
-        ch[n] = 1
-        dfs(n + 1, d, nd + 1)
-        # 사용하지 않는 경우
-        ch[n] = 0
-        dfs(n + 1, d, nd)
+    # if sum(1 if ch[i] else 0 for i in range(n)) + len(ch[n:]) < d: #컷에지
+    #     return
+
+    else:
+        for i in range(len(innum)):
+            if ch[i] is None:
+                ch[i] = nd
+                dfs(d, nd+1)
+                ch[i] = None
 
 
 def primenum(n):
-    if n == 0:
+    if n in range(2):
         return False
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
@@ -41,24 +41,25 @@ def solution(numbers):
     global innum
 
     innum = list(numbers)
-    ch = [0] * len(innum)
+    ch = [None] * len(innum)
     numlist = []
 
     for i in range(1, len(innum) + 1):
-        dfs(0, i, 0)
+        dfs(i, 0)
 
-    numlist = list(map(int, numlist))
+
     numlist = list(set(numlist))
 
     i = 0
     while i < len(numlist):
         if not primenum(numlist[i]):
             del numlist[i]
-        i += 1
+        else:
+            i += 1
 
     answer = len(numlist)
     return answer
 
 if __name__ == '__main__':
-    n = '7843'
+    n = '011'
     print(solution(n))
